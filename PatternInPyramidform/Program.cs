491c63adfec69bdfba513d675e4efc724ff897fd
samikshaghuge5 <samikshaghuge.sitcomp@gmail.com>
2023-01-17 16:29:09 +0530

    Add project files.

---
+++ PatternInPyramidform/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternInPyramidform
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //star pattern in pyramid form
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            //numbers in pyramid form
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            //numbers in reverse pyramid
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            //numbers in reverse form in reverse pyramid
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 5; j >= i; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            //
+        }
+    }
+}
